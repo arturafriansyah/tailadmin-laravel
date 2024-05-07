6eba3064fb955383abe81af1ae3a1c652d3c5feb
arturafriansyah <pertamax134@gmail.com>
2024-05-07 04:27:18 +0000

    initial commit

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cp .env.example .env
 php artisan key:generate
 
 sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.1/g' .env &&
-sed -i 's/DB_DATABASE=test/DB_DATABASE=tailadmin' .env &&
+sed -i 's/DB_DATABASE=test/DB_DATABASE=tailadmin/g' .env &&
 sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env &&
 
 php artisan migrate
